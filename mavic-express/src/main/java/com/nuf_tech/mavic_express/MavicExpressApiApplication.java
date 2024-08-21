d03e9a0abf051fce4899a8f57f8e36bac4efe65a
Ivan Borodinov <ivan.borodinov.03@mail.ru>
2024-08-21 12:11:25 +0300

    first commit

---
+++ mavic-express/src/main/java/com/nuf_tech/mavic_express/MavicExpressApiApplication.java
@@ -0,0 +1,13 @@
+package com.nuf_tech.mavic_express;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavicExpressApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavicExpressApiApplication.class, args);
+	}
+
+}
---
+++ mavic-express/src/test/java/com/nuf_tech/mavic_express/MavicExpressApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuf_tech.mavic_express;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavicExpressApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
